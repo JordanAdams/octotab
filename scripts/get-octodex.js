d43545d002d5662dd79a4b31b48173e83d5b580e
Jordan Adams <hi@jordanadams.co.uk>
2016-12-12 00:00:08 +0000

    Adds linting

--- scripts/get-octodex.js
+++ scripts/get-octodex.js
@@ -10,7 +10,7 @@ const getFeedEntries = pathOr([], ['feed', 'entry']);
 
 const transformEntry = entry => ({
   title: entry.title[0],
-  image_url: entry.content[0].div[0].a[0].img[0].$.src,
+  image_url: entry.content[0].div[0].a[0].img[0].$.src
 });
 
 request('http://feeds.feedburner.com/Octocats')
@@ -20,4 +20,4 @@ request('http://feeds.feedburner.com/Octocats')
   .then(octodex => JSON.stringify(octodex, null, 2))
   .then(json => fs.writeFile(outputPath, json))
   .then(() => console.log(`Wrote octodex to ${outputPath}`))
-  .catch(err => console.error(err))
+  .catch(err => console.error(err));
